3440c3280029c696bf399fdb036137b0f1c27e89
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-12-27 18:05:27 +0530

    added menuItems placeholder to dynamically remove access to navigation

--- Farmers Market/Farmers Market/Site.Master.designer.cs
+++ Farmers Market/Farmers Market/Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace Farmers_Market
     {
 
         /// <summary>
+        /// menuItems control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder menuItems;
+
+        /// <summary>
         /// userAvatar control.
         /// </summary>
         /// <remarks>
